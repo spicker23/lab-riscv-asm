c1bb4e55e9179a7ef87253a948f3c6efe3d15cb6
Konstantin Zhvakin <spicker23@yandex.ru>
2021-08-23 17:50:22 +0500

    C program changed

--- example.c
+++ example.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
-#define N 3
+#define N 5
 #define M 4
 
-void process(int n, int m, int matrix [M][N], int *result)
-{
+void process(int n, int m, int matrix [M][N], int min) {
+    min = matrix[0][0];
     for (int i = 0; i < m; ++i) {
-        result[i] = 0;
         for (int j = 0; j < n; ++j) {
-            result[i] += matrix[i][j];
+            if (matrix [i][j] <= min)
+            min = matrix[i][j];
         }
     }
-
+    printf("%d ", min);
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     int matrix[M][N];
-    int result[M];
+    int min;
 
     for (int i = 0; i < M; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -25,11 +24,7 @@ int main(int argc, char** argv)
         }
     }
 
-    process(N, M, matrix, result);
-
-    for (int i = 0; i < M; ++i) {
-        printf("%d ", result[i]);
-    }
+    process(N, M, matrix, min);
 
     return 0;
 }
